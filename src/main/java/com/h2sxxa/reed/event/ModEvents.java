7ce762e749337bc918fe37943dc47c521b2d3aa9
IAXRetailer <retailer673@163.com>
2022-08-04 10:56:35 +0800

    add reed_seed

--- src/main/java/com/h2sxxa/reed/event/ModEvents.java
+++ src/main/java/com/h2sxxa/reed/event/ModEvents.java
@@ -1,13 +1,30 @@
 package com.h2sxxa.reed.event;
 
-import com.h2sxxa.reed.Main;
+import java.util.Random;
 
+import com.h2sxxa.reed.Main;
+import com.h2sxxa.reed.init.ModItem;
+
+import net.minecraft.block.BlockTallGrass;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = Main.MODID)
 public final class ModEvents {
+    private static Random EventRandom = new Random();
 /*
  * @SubscribeEvent
  * public static void onEventDeath(LivingDeathEvent event){}
  */
+    @SubscribeEvent
+    public static void DropSeed(HarvestDropsEvent event) {
+        if(event.getState().getBlock() instanceof BlockTallGrass)
+        {
+            if (EventRandom.nextInt(7) == 0) {
+                event.getDrops().add(new ItemStack(ModItem.REED_SEED));
+            }
+        }
+    }
 }
--- src/main/java/com/h2sxxa/reed/init/ModItem.java
+++ src/main/java/com/h2sxxa/reed/init/ModItem.java
@@ -16,6 +16,7 @@ public class ModItem {
     //public static final Item MISCANTHUS_SACCHARIFLORUS = new InfoDescBase("miscanthus_sacchariflorus", FoodTab);
     //AgriTab
     public static final Item REED = new InfoDescBase("reed", AgriTab.AgriTab);
+    public static final Item REED_SEED = new InfoDescBase("reed_seed", AgriTab.AgriTab);
     //FoodTab
     public static final Item REED_COOKIE = new InfoFoodBase("reed_cookie",5,5f,false,FoodTab.FoodTab);
 
